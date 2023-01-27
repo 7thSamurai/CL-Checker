073194753473e2a08d11c2fec1aeb163a59e5e75
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-01-27 16:33:31 -0500

    Change config to be stored in AppData

--- src/config.py
+++ src/config.py
@@ -1,11 +1,37 @@
 import json, os
 
 class Config:
-    def __init__(self):    
-        self.path = os.path.join(os.path.dirname(__file__), 'config.json')
+    def __init__(self):
+        # Setup the default config values
+        self.db_path = 'checker.db'
+        self.update_secs = 300
+        self.auto_start = False
+        self.from_email = ''
+        self.from_password = ''
+        self.to_email = ''
+    
+        # Find the AppData path
+        self.appdata = os.path.join(os.getenv('LOCALAPPDATA'), 'CL-Checker')
+        
+        # Make sure the the AppData directory exists
+        if not os.path.isdir(self.appdata):
+            try:
+                os.mkdir(self.appdata)
+            except:
+                print(f'Unable to create directory {self.appdata}')
+        
+        # Generate the config path
+        self.config_path = os.path.join(self.appdata, 'config.json')
+        
+        # Load the data from the config if it exists, otherwise save the default values
+        if self.path.exists(self.config_path):
+            self.load()
+        else:
+            self.save()
 
+    def load(self):
         # Load the config data from a JSON file
-        with open(self.path, 'r') as f:
+        with open(self.config_path, 'r') as f:
             data = json.load(f)
 
         self.db_path = data['db_path']
@@ -26,7 +52,7 @@ class Config:
         }
 
         # Write the config data to a JSON file
-        with open(self.path, 'w') as f:
+        with open(self.config_path, 'w') as f:
             json.dump(data, f, indent=4)
 
 config = Config()
